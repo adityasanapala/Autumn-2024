19576363f81361e4fda362d25fa567d89e6dad14
Aditya Sanapala <wickedsan106@gmail.com>
2024-08-22 16:19:45 +0530

    Create q03.cpp

---
+++ CS213/Sample Code/Stack/q03.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#include <stack>
+#define LOG(x) cout << x << endl;
+
+// Question: Given a stack, the task is to delete the middle element of it without using any additional data structures.
+// Example Input: Stack[] = [1, 2, 3, 4, 5]
+// Example Output: Stack[] = [1, 2, 4, 5]
+// Example Input: Stack[] = [1, 2, 3, 4, 5, 6]
+// Example Output: Stack[] = [1, 2, 4, 5, 6]
