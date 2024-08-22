677fc2eb659996466d3b0ea05b6c13c8aebfd96d
Aditya Sanapala <wickedsan106@gmail.com>
2024-08-22 16:56:15 +0530

    added solution

--- CS213/Sample Code/Stack/q03.cpp
+++ CS213/Sample Code/Stack/q03.cpp
@@ -8,3 +8,40 @@ using namespace std;
 // Example Output: Stack[] = [1, 2, 4, 5]
 // Example Input: Stack[] = [1, 2, 3, 4, 5, 6]
 // Example Output: Stack[] = [1, 2, 4, 5, 6]
+
+void delete_mid(stack<int> &s) {
+    vector<int> v;
+
+    while (!s.empty()) {
+        v.push_back(s.top());
+        s.pop();
+    }
+
+    int n = v.size();
+
+    int mid_index = (n - 1) / 2;
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (i != mid_index) {
+            s.push(v[i]);
+        }
+    }
+}
+
+int main() {
+
+    stack<int> s;
+    s.push(1);
+    s.push(3);
+    s.push(7);
+
+    delete_mid(s);
+
+    while (!s.empty())
+    {
+        cout << s.top() << endl;
+        s.pop();
+    }
+
+    return 0;
+}
