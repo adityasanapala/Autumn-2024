a2ea002653ca42468cc60a03e3f2fbd04bfa7b2a
Aditya Sanapala <wickedsan106@gmail.com>
2024-08-22 14:41:41 +0530

    added solution

--- CS213/Sample Code/Stack/q02.cpp
+++ CS213/Sample Code/Stack/q02.cpp
@@ -7,8 +7,35 @@ using namespace std;
 // Example Input : Hello World
 // Example Output : olleH dlroW
 
+string reverse_word(string s) {
+    string ans;
+    stack<char> rev;
+
+    for (char c : s) {
+        if (c == ' ') {
+            while (!rev.empty()) {
+                ans.push_back(rev.top());
+                rev.pop();
+            }
+            ans.push_back(c);
+        } else {
+            rev.push(c);
+        }
+    }
+
+    while (!rev.empty()) {
+        ans.push_back(rev.top());
+        rev.pop();
+    }
+
+    return ans;
+}
+
 int main() {
+    string s;
+    getline(cin, s);
 
+    string ans = reverse_word(s);
 
-  return 0;
+    cout << ans << endl;
 }
