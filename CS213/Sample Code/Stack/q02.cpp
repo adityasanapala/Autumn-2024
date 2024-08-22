73c3baaadb3550a6bf565c8a5091c6481839d7df
Aditya Sanapala <wickedsan106@gmail.com>
2024-08-22 08:53:50 +0530

    Create q02.cpp

---
+++ CS213/Sample Code/Stack/q02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <stack>
+#define LOG(x) cout << x << endl;
+
+// Question: Given string str, we need to print the reverse of individual words.
+// Example Input : Hello World
+// Example Output : olleH dlroW
+
+int main() {
+
+
+  return 0;
+}
