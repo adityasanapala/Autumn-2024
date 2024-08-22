84df1dc038c82b2acc147cb9f8381c523990c9db
Aditya Sanapala <wickedsan106@gmail.com>
2024-08-22 16:26:38 +0530

    completed code

--- CS213/Sample Code/Stack/q01.cpp
+++ CS213/Sample Code/Stack/q01.cpp
@@ -4,9 +4,34 @@ using namespace std;
 #define LOG(x) cout << x << endl;
 
 // Question: Given a string, reverse it using a stack.
+// Example Input: s = "Hello"
+// Example Output: s = "olleH"
+
+string reverse(string s)
+{
+    string ans;
+    stack<char> rev;
+
+    for (char c : s)
+    {
+        rev.push(c);
+    }
+
+    while (!rev.empty())
+    {
+        ans.push_back(rev.top());
+        rev.pop();
+    }
+
+    return ans;
+}
 
 int main() {
+    string s;
+    cin >> s;
 
+    string ans = reverse(s);
+    cout << ans << endl;
 
-  return 0;
+    return 0;
 }
