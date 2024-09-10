6188a4857b1a454a156f8ced2893d18914f2333e
Aditya Sanapala <wickedsan106@gmail.com>
2024-09-11 01:37:20 +0530

    Create hello.c

---
+++ CS231/Sample Code/C/hello.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
+
+// This was the original "hello, world" code written by Brian Kernighan, the creator of C, in 1978.
+
+// main()
+// {
+//     printf("hello, world\n");
+// }
